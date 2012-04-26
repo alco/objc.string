5452ed80dd964ee8d4d34fe851ad2389dfb0fda1
Alexei Sholik <alcosholik@gmail.com>
2012-04-26 15:01:46 +0300

    Add prototypes for future methods

--- objc.string/NSString+ObjCString.h
+++ objc.string/NSString+ObjCString.h
@@ -15,19 +15,30 @@
 - (NSString *)append:(NSString *)str;
 - (NSString *)capitalize;
 - (NSString *)compress;
-- (NSString *)delete:(NSRange)range;  // splice:range with:@""
+- (NSString *)cut:(NSRange)range;                   // <--
+- (NSString *)cutFrom:(NSUInteger)position;         // <--
+- (NSString *)cutTo:(NSUInteger)position;           // <--
+- (NSString *)delete:(NSRange)range;
 - (NSString *)expandTabs;  // -expandTabs:4
 - (NSString *)expandTabs:(NSUInteger)tabsize;
-- (NSString *)insert:(NSString *)str at:(NSUInteger)position;  // splice:NSMakeRange(position, 0) with:str
+- (NSString *)filter:(NSString *)str;               // <--
+- (NSString *)filterChars:(NSCharacterSet *)chars;  // <--
+- (NSString *)insert:(NSString *)str at:(NSUInteger)position;
 - (NSString *)lowercase;
+- (NSString *)pad:(NSUInteger)length;
+- (NSString *)pad:(NSUInteger)length with:(NSString *)str;
+- (NSString *)pad:(NSUInteger)length with:(NSString *)str flags:(int)flags;
 - (NSString *)repeat:(NSUInteger)count;
 - (NSString *)repeat:(NSUInteger)count sep:(NSString *)separator;
 - (NSString *)replace:(NSString *)substr with:(NSString *)repl;
+- (NSString *)reverse;                              // <--
 - (NSString *)splice:(NSRange)range with:(NSString *)str;
 - (NSString *)titlecase;
 - (NSString *)ltrim;
 - (NSString *)rtrim;
 - (NSString *)trim;
+- (NSString *)truncate;                             // <--
+- (NSString *)truncate:(int)flags;                  // <--
 - (NSString *)uppercase;
 
 /*
@@ -60,5 +71,22 @@
 
 - (NSUInteger)count:(NSString *)substr;
 - (BOOL)isBlank;
+- (BOOL)has:(NSString *)substr;                // <--
+- (BOOL)hasChars:(NSCharacterSet *)chars;      // <--
+- (BOOL)hasOnlyChars:(NSCharacterSet *)chars;  // <--
+
+- (BOOL)isAlpha;                               // <--
+- (BOOL)isDigit;                               // <--
+- (BOOL)isAlnum;                               // <--
+
+/*
+ * Convenience methods that wrap the standard NSString methods in a more
+ * concise interface.
+ */
+- (NSUInteger)find:(NSString *)substr;
+- (NSUInteger)find:(NSString *)substr in:(NSRange)range;
+- (NSUInteger)rfind:(NSString *)substr;
+- (NSUInteger)rfind:(NSString *)substr in:(NSRange)range;
+
 
 @end
